d823077bd7705b30a4102b3b850acbe73e56856d
BatchClayderman <goosebt.universe@gmail.com>
2025-01-14 23:54:18 +0800

    Update (2025011423)

--- src/customize.sh
+++ src/customize.sh
@@ -3,8 +3,9 @@ EXIT_SUCCESS=0
 EXIT_FAILURE=1
 EOF=255
 moduleName=Bypasser
-cd "${MODPATH}"
 ui_print "Welcome to the installer of the ${moduleName} Magisk Module! "
+cd "${MODPATH}"
+ui_print "Current working directory is \"$(pwd)\". "
 
 # Magisk #
 if ! $BOOTMODE;
@@ -24,15 +25,12 @@ else
 fi
 if [[ -z "${MAGISK_VER_CODE}" ]];
 then
-	ui_print "Warning: Cannot guarantee whether the action button is supported. "
-	ui_print "Warning: Maybe you can update configurations only by flashing a newer version. "
+	ui_print "Warning: Cannot guarantee whether the action button is supported. Maybe you can update configurations only by flashing a newer version. "
 else
 	if [[ ${MAGISK_VER_CODE} -ge 28000 ]];
 	then
 		ui_print "The action button is supported (${MAGISK_VER_CODE}). "
 	else
-		ui_print "Warning: The action button is not supported (${MAGISK_VER_CODE}). "
-		ui_print "Warning: Maybe you can update configurations only by flashing a newer version. "
-		ui_print "Warning: Please try to use the latest manager (Magisk: \${MAGISK_VER_CODE}>= 28000). "
+		ui_print "Warning: The action button is not supported (${MAGISK_VER_CODE}). Maybe you can update configurations only by flashing a newer version. Please try to use the latest manager (Magisk: \${MAGISK_VER_CODE}>= 28000). "
 	fi
 fi
