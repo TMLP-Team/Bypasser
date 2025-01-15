e71152a52282be7c7f9fa3cb42b93089548bdf28
BatchClayderman <goosebt.universe@gmail.com>
2025-01-16 01:29:02 +0800

    Update

--- src/customize.sh
+++ src/customize.sh
@@ -1,7 +1,7 @@
 #/system/bin/sh
-EXIT_SUCCESS=0
-EXIT_FAILURE=1
-EOF=255
+readonly EXIT_SUCCESS=0
+readonly EXIT_FAILURE=1
+readonly EOF=255
 moduleName=Bypasser
 cd "${MODPATH}"
 ui_print "Welcome to the installer of the ${moduleName} Magisk Module! "
