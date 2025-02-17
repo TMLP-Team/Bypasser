e6cac0299f05e719589bbcc1ce19c56275ac419f
BatchClayderman <goosebt.universe@gmail.com>
2025-02-17 21:42:10 +0800

    Module Update (2025021721)

--- src/actionB.sh
+++ src/actionB.sh
@@ -549,7 +549,7 @@ readonly zygiskNextConfigurationFolderPath="../../zygisksu"
 readonly zygiskNextDenylistConfigurationFileName="denylist_enforce"
 readonly zygiskNextDenylistConfigurationFilePath="${zygiskNextConfigurationFolderPath}/${zygiskNextDenylistConfigurationFileName}"
 
-if [[ -d "${shamikoInstallationFolderPath}" && -n "$(find "${shamikoInstallationFolderPath}" -maxdepth 1 -mindepth 1 -print -quit)" ]];
+if [ -d "${shamikoInstallationFolderPath}" ] && [ -n "$(ls -1A ${shamikoInstallationFolderPath})" ];
 then
 	echo "The shamiko installation folder was found at \"${shamikoInstallationFolderPath}\". "
 	if [[ ! -d "${shamikoConfigurationFolderPath}" || -z "$(ls -1A "${shamikoConfigurationFolderPath}")" ]];
@@ -569,7 +569,7 @@ then
 else
 	echo "No shamiko installation folders were found. "
 fi
-if [[ -d "${zygiskNextConfigurationFolderPath}" && -n "$(find "${zygiskNextConfigurationFolderPath}" -maxdepth 1 -mindepth 1 -print -quit)" ]];
+if [ -d "${zygiskNextConfigurationFolderPath}" ] && [ -n "$(ls -1A ${zygiskNextConfigurationFolderPath})" ];
 then
 	echo "The Zygisk Next configuration folder was found at \"${zygiskNextConfigurationFolderPath}\". "
 	echo -n 1 > "${zygiskNextDenylistConfigurationFilePath}"
